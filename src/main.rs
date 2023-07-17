6d722cd27f1560b060a664ac2d9ec09fc855566a
Lilit0x <baasisek01@gmail.com>
2023-07-17 23:26:43 +0100

    chore: 0.0.9 release

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod logger;
 #[derive(Parser, Debug)]
 #[command(name = "share")]
 #[command(author = "Wokebuild. <woke.build>")]
-#[command(version = "0.0.8")]
+#[command(version = "0.0.9")]
 #[command(about = "Share anything with teammates across machines via CLI.", long_about = None)]
 pub struct Cli {
     /// Separated list of secrets to share. Key-Value pair is seperated by a comma. "my_key,my_value"
